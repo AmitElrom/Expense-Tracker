8c3138494f95e2c70aaa799f70f15249612d9550
Amit Elrom <amitelrom99@gmail.com>
2022-05-12 19:34:01 +0300

    updating form

--- src/components/New Expense Folder/Expense Form/ExpenseForm.js
+++ src/components/New Expense Folder/Expense Form/ExpenseForm.js
@@ -2,35 +2,70 @@ import React, { useState } from 'react'
 import Card from '../../UI/Card/Card'
 import './ExpenseForm.css'
 
+// form management libraries
+import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+const required = 'is a required field'
+
+const schema = yup.object().shape({
+    title: yup.string().required(`Title ${required}`),
+    amount: yup.number().typeError(`Amount ${required}`).required(),
+    date: yup.string().required(`Date ${required}`)
+})
+
 const ExpenseForm = () => {
 
-    const [newExpense, setNewExpense] = useState({
-        title: '',
-        amount: '',
-        date: ''
+    const { register, handleSubmit, setValue, formState: { errors } } = useForm({
+        resolver: yupResolver(schema)
     })
 
+    const [newExpense, setNewExpense] = useState({ title: '', amount: '', date: '' })
+    const { title, amount, date } = newExpense
+
     const changeHandler = (e) => {
-        const { id, value } = e.target;
-        setNewExpense({ ...newExpense, id: value })
+        const { name, value } = e.target;
+
+        if (name === 'date') {
+            setNewExpense((newExpense) => {
+                return { ...newExpense, date: new Date(value) }
+            })
+        }
+        setNewExpense((newExpense) => {
+            return { ...newExpense, [name]: value }
+        })
+
         console.log(newExpense);
+    }
+
+    const submitForm = (obj) => {
+        console.log(obj);
+
+        // delete data from form's inputs
+        setValue('title', '')
+        setValue('amount', '')
+        setValue('date', '')
     }
 
     return (
         <Card>
-            <form>
+            <form onSubmit={handleSubmit(submitForm)} >
                 <div className='new-expense__controls' >
                     <div className='new-expense__control' >
                         <label htmlFor='title' >Title</label>
-                        <input id='title' type='text' onChange={changeHandler} />
+                        <input id='title' defaultValue={title} name='title' type='text' onChange={changeHandler} {...register('title')} />
+                        <p>{errors.title?.message}</p>
                     </div>
                     <div className='new-expense__control' >
                         <label htmlFor='amount' >Amount</label>
-                        <input id='amount' type='number' min='1' onChange={changeHandler} />
+                        <input id='amount' defaultValue={amount} name='amount' type='number' min='1' onChange={changeHandler} {...register('amount')} />
+                        <p>{errors.amount?.message}</p>
                     </div>
                     <div className='new-expense__control' >
                         <label htmlFor='date' >Date</label>
-                        <input id='date' type='date' min='2018-01-01' max='2022-12-31' onChange={changeHandler} />
+                        <input id='date' defaultValue={date} name='date' type='date' min='2018-01-01' max='2022-12-31' onChange={changeHandler} {...register('date')} />
+                        <p>{errors.date?.message}</p>
                     </div>
                 </div>
                 <div className='new-expense__actions' >
