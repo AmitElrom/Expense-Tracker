c4ed3ceb1a95b1b7a20a331aca016d3b66ea0672
Amit Elrom <amitelrom99@gmail.com>
2022-05-12 10:56:21 +0300

    card style apply

--- src/components/Expense Item/ExpenseItem.js
+++ src/components/Expense Item/ExpenseItem.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import './ExpenseItem.css'
 import ExpenseDate from '../Expense Date/ExpenseDate'
+import Card from '../Card/Card'
 
 const ExpenseItem = ({ expense }) => {
 
     const { title, amount } = expense
 
     return (
-        <div className='expense-item' >
+        <Card className='expense-item' >
             <ExpenseDate date={expense.date} />
             <div className='expense-item__description' >
                 <h2>{title}</h2>
             </div>
             <div className='expense-item__price' >{amount}</div>
-        </div>
+        </Card>
     )
 }
 
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import ExpenseItem from '../Expense Item/ExpenseItem'
+import './Expenses.css'
+import Card from '../Card/Card'
 
 const Expenses = ({ expenses }) => {
     return (
-        <div>
+        <Card className='expenses' >
             <h2>Expenses</h2>
             <ExpenseItem expense={expenses[0]} />
             <ExpenseItem expense={expenses[1]} />
             <ExpenseItem expense={expenses[2]} />
-        </div>
+        </Card>
     )
 }
 
