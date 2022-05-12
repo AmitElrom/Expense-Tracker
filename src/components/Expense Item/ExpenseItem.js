18aef6c48ddb8b14c352dd615d432608379c0b92
Amit Elrom <amitelrom99@gmail.com>
2022-05-12 09:54:11 +0300

    using props and css

--- src/components/Expense Item/ExpenseItem.js
+++ src/components/Expense Item/ExpenseItem.js
@@ -3,9 +3,16 @@ import './ExpenseItem.css'
 import ExpenseDate from '../Expense Date/ExpenseDate'
 
 const ExpenseItem = ({ expense }) => {
+
+    const { title, amount } = expense
+
     return (
-        <div>
+        <div className='expense-item' >
             <ExpenseDate date={expense.date} />
+            <div className='expense-item__description' >
+                <h2>{title}</h2>
+            </div>
+            <div className='expense-item__price' >{amount}</div>
         </div>
     )
 }
